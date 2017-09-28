ecad23361eb9989241206bbe476685ebf622b9da
AlisonAO <alison_opeloyeru@hotmail.co.uk>
2017-09-29 00:16:44 +0100

    updated recipes

--- controllers/recipe_controller.rb
+++ controllers/recipe_controller.rb
@@ -9,43 +9,97 @@ class RecipeController < Sinatra::Base
 	set :views, Proc.new { File.join(root, 'views') }
 
 	$recipes = [{
-	      title: "Apple Pie",
-	      ingredients: "word",
-	      body: "Method
-				1. For the pastry, place the flour, sugar and lemon zest into a bowl and rub in the butter until the mixture resembles breadcrumbs. Add the beaten egg and stir with a round-bladed knife until the mixture forms a dough.
-				2. Set aside one-third of the pastry for the lid.
-				3. Roll out the remaining pastry on a lightly floured surface until the thickness of a pound coin and 5-7cm/2-3in larger than the pie dish. Lift the pastry over the rolling pin and lower it gently into the pie dish.
-				4. Press the pastry firmly into the dish and up the sides, making sure there are no air bubbles. Chill the fridge for a few minutes.
-				5. Preheat the oven to 200C/180 (fan)/Gas 6. Place a baking tray into the oven to preheat.
-				6. For the filling, mix the sugar, cinnamon and cornflour in a large bowl. Stir in the apples.
-				7. Place the apple filling into the pie dish, making sure that it rises above the edge. Brush the rim of the dish with beaten egg.
-				8. Roll out the reserved ball of pastry. Cover the pie with the pastry and press the edges together firmly to seal. Using a sharp knife, trim off the excess pastry, then gently crimp all around the edge. Make a few small holes in the centre of the pie with the tip of a knife. Glaze the top with beaten egg.
-				9. Lightly knead the pastry trimmings and re-roll. Cut into leaf shapes place all around the edge of the pie, slightly overlapping each other, and glaze with more egg. Sprinkle the pie with sugar and bake in the centre of the oven for 45–55 minutes or golden-brown all over and the apples are tender."
+	      title: "Strawberry Nutella Smores",
+	      ingredients: "1 marshmallow,
+	      				½ teaspoon nutella,
+	       				2 lemon thins,
+	       				2 strawberry slices",
+	      body: "Toast marshmallow over a fire. Spread chocolate-hazelnut spread on one lemon thin; top with the marshmallow, strawberry and the second lemon thin."
 	  },
 	  {
-	      title: "Post 2",
-	      ingredients: "word",
-	      body: "This is the second post"
+	      title: "Five-Spice Tilapia",
+	      ingredients: "1 pound tilapia fillets
+				 1 teaspoon Chinese five-spice powder, ¼ cup reduced-sodium soy sauce, 3 tablespoons light brown sugar, 1 tablespoon canola oil3 scallions, thinly sliced
+				 Onions Green Bunch",
+	      body: "1. Sprinkle both sides of tilapia fillets with five-spice powder. Combine soy sauce and brown sugar in a small bowl. 
+	      		 2. Heat oil in a large nonstick skillet over medium-high heat. Add the tilapia and cook until the outer edges are opaque, about 2 minutes. Reduce heat to medium, turn the fish over, stir the soy mixture and pour into the pan. Bring the sauce to a boil and cook until the fish is cooked through and the sauce has thickened slightly, about 2 minutes more. Add scallions and remove from the heat. Serve the fish drizzled with the pan sauce."
 	  },
 	  {
-	      title: "Post 3",
-	      ingredients: "word",
-	      body: "This is the second post"
+	      title: "Chinese Fried Rice",
+	      ingredients: "3⁄4 cup finely chopped onion,
+						2 1⁄2 tablespoons oil,
+						1 egg, lightly beaten (or more eggs if you like),
+						3 drops soy sauce,
+						3 drops sesame oil,
+						8 ounces cooked lean boneless pork or 8 ounces chicken, chopped,
+						1⁄2 cup finely chopped carrot (very small),
+						1⁄2 cup frozen peas, thawed,
+						4 cups cold cooked rice, grains separated (preferably medium grain),
+						4 green onions, chopped,
+						2 cups bean sprouts,
+						2 tablespoons light soy sauce (add more if you like)",
+	      body: "1. Heat 1 tbsp oil in wok; add chopped onions and stir-fry until onions turn a nice brown color, about 8-10 minutes; remove from wok.
+				 2.Allow wok to cool slightly.
+				 3. Mix egg with 3 drops of soy and 3 drops of sesame oil; set aside.
+				 4. Add 1/2 tbsp oil to wok, swirling to coat surfaces; add egg mixture; working quickly, swirl egg until egg sets against wok; when egg puffs, flip egg and cook other side briefly; remove from wok, and chop into small pieces.
+				 5. Heat 1 tbsp oil in wok; add selected meat to wok, along with carrots, peas, and cooked onion; stir-fry for 2 minutes.
+				 6. Add rice, green onions, and bean sprouts, tossing to mix well; stir-fry for 3 minutes.
+				 7. Add 2 tbsp of light soy sauce and chopped egg to rice mixture and fold in; stir-fry for 1 minute more; serve.
+				 8. Set out additional soy sauce on the table, if desired."
 	  },
 	  {
-	      title: "Post 4",
-	      ingredients: "word",
-	      body: "This is the second post"
+	      title: "Cinamon Cookies",
+	      ingredients: "1 cup sugar,
+						1⁄2 cup butter,
+						1 large egg,
+						1 teaspoon vanilla,
+						1 1⁄2 cups flour,
+						1 1⁄2 teaspoons cinnamon,
+						1 teaspoon baking powder,
+						1⁄4 teaspoon salt,
+						cinnamon sugar",
+	      body: "1. In a mixer bowl, cream together sugar and butter; beat in egg and vanilla.
+				 2. Combine flour, cinnamon, baking powder and salt.
+				 3. Add to butter mixture.
+				 4. Blend well.
+				 5. Cover and refrigerate 2 hours or till firm enough to roll into balls.
+				 6. Shape dough into small balls about 3/4-inch in diameter.
+				 7. Roll in cinnamon sugar to coat.
+				 8. Set cookies 1-inch apart on lightly greased cookie sheets.
+				 9. Bake at 350° for 10 minutes or till the edges are lightly browned.
+				 10. Cool slightly on pans, then remove to racks to cool completely."
 	  },
 	  {
-	      title: "Post 5",
-	      ingredients: "word",
-	      body: "This is the second post"
+	      title: "Shrimp Veracruzana",
+	      ingredients: "2 teaspoons canola oil,
+	      				1 bay leaf1 medium onion, halved and thinly sliced,
+						2 jalapeño peppers, seeded and very thinly sliced, or to taste4 cloves garlic, minced
+						1 pound peeled and deveined raw shrimp, (16-20 per pound,
+						3 medium tomatoes, diced¼ cup thinly sliced pitted green olives
+						1 lime, cut into 4 wedges",
+	      body: "1. Heat oil in a large nonstick skillet over medium heat. 
+	      		 2. Add bay leaf and cook for 1 minute. 
+	      		 3. Add onion, jalapenos and garlic and cook, stirring, until softened, about 3 minutes. 
+	      		 4. Stir in shrimp, cover and cook until pink and just cooked through, 3 to 4 minutes. 
+	      		 5. Stir in tomatoes and olives. Bring to a simmer, reduce heat to medium-low, replace cover and cook until the tomatoes are almost broken down, 2 to 3 minutes more. 
+	      		 6. Remove the bay leaf. Serve with lime wedges."
 	  },
 	  {
-	      title: "Post 6",
-	      ingredients: "word",
-	      body: "This is the third post"
+	      title: "Crab Cake Burgers",
+	      ingredients: "1 pound crabmeat,
+	      				1 egg, lightly beaten,
+	      				½ cup panko breadcrumbs,
+	      				¼ cup light mayonnaise,
+						2 tablespoons minced chives,
+						1 tablespoon Dijon mustard1 tablespoon lemon juice,
+						1 teaspoon celery seed,
+						1 teaspoon onion powder,
+						¼ teaspoon freshly ground pepper,
+						4 dashes hot sauce, 
+						1 tablespoon extra-virgin olive oil,
+						2 teaspoons unsalted butter",
+	      body: "1. Mix crab, egg, breadcrumbs, mayonnaise, chives, mustard, lemon juice, celery seed, onion powder, pepper and hot sauce in a large bowl. Form into 6 patties. 
+	      		 2. Heat oil and butter in a large nonstick skillet over medium heat until the butter stops foaming. Cook the patties until golden brown, about 4 minutes per side."
 	  }]
 
 	  get '/recipes' do 
@@ -60,7 +114,7 @@ class RecipeController < Sinatra::Base
 
 
 	get '/recipes/:id' do 
-		@page_header = "Specific Post"
+		@page_header = "Read the full recipe below:"
 		id = params[:id].to_i 
 		@recipes = $recipes[id]
 		erb :"recipes/show" 
